6534ce0464591110c30892562dce9d5b13e933cc
A Ramanuj Patro <ramanujpatro07@gmail.com>
2025-12-03 05:10:11 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,33 @@
+// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { QuizProvider } from './context/QuizContext';
 import Home from './pages/Home';
 import CreateQuiz from './pages/CreateQuiz';
 import JoinQuiz from './pages/JoinQuiz';
 import QuizSession from './pages/QuizSession';
 import Leaderboard from './pages/Leaderboard';
 import AdminDashboard from './pages/AdminDashboard';
+import Navbar from './components/Navbar';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-100">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/create-quiz" element={<CreateQuiz />} />
-          <Route path="/join-quiz" element={<JoinQuiz />} />
-          <Route path="/quiz/:sessionId" element={<QuizSession />} />
-          <Route path="/leaderboard/:sessionId" element={<Leaderboard />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-        </Routes>
-      </div>
-    </Router>
+    <QuizProvider>
+      <Router>
+        <div className="min-h-screen bg-gradient-to-br from-indigo-50 to-blue-100">
+          <Navbar />
+          <Toaster position="top-right" />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<CreateQuiz />} />
+            <Route path="/join" element={<JoinQuiz />} />
+            <Route path="/quiz/:sessionId" element={<QuizSession />} />
+            <Route path="/leaderboard/:sessionId" element={<Leaderboard />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+          </Routes>
+        </div>
+      </Router>
+    </QuizProvider>
   );
 }
 
